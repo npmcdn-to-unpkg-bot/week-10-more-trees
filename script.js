8fb81fc4ff383bfe0bca6a5290857bb8a2b983b7
Chuck Pedro <Chuck Pedro>
2016-08-09 19:53:48 -0400

    putting circle pack in a framed page

---
+++ script.js
@@ -0,0 +1,96 @@
+var margin = 20,
+    diameter = 960;
+
+var color = d3.scale.category20c();
+
+var pack = d3.layout.pack()
+    .padding(1)
+    .size([diameter - margin, diameter - margin])
+    .value(function(d) { return d.size; })
+
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter + 100)
+    .attr("height", diameter)
+  .append("g")
+    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");   
+
+var focus;
+
+      var nodes,
+      view;
+
+d3.json("exports.json", function(error, root) {
+  if (error) throw error;
+    focus = root;
+    Chart(root);
+    
+});            
+
+
+    
+
+
+    function Chart (root) {
+        
+        var chart = this;     
+//        chart.data = focus;
+        
+
+
+
+        
+nodes = pack.nodes(root);      
+
+
+  var circle = svg.selectAll("circle")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+  var text = svg.selectAll("text")
+      .data(nodes)
+    .enter().append("text")
+      .attr("class", "label")
+//      .attr('transform', 'rotate(45deg)')
+//      .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+      .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
+      .text(function(d) { return d.name + ', ' + d.value; })
+  ;
+
+  var node = svg.selectAll("circle,text");
+
+  d3.select("body")
+//      .style("background", color(-1))
+      .on("click", function() { zoom(root); });
+
+  zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+  function zoom(d) {
+    var focus0 = focus; focus = d;
+
+    var transition = d3.transition()
+        .duration(d3.event.altKey ? 7500 : 250) // last number is transition speed
+        .tween("zoom", function(d) {
+          var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+          return function(t) { zoomTo(i(t)); };
+        });
+
+    transition.selectAll("text")
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+        .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+        .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+        .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  function zoomTo(v) {
+    var k = diameter / v[2]; view = v;
+    node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+    circle.attr("r", function(d) { return d.r * k; });
+  }
+
+}
+
+//d3.select(self.frameElement).style("height", diameter + "px");
+
